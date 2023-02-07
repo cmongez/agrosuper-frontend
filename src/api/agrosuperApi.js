e2205873f6a24afec4abda99ade1c9d7d93053ff
Cesar <camongez28@gmail.com>
2023-02-07 14:30:46 -0300

    modificando la api http por https

--- src/api/agrosuperApi.js
+++ src/api/agrosuperApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = process.env.REACT_APP_API_URL || 'http://f11.cl:8090';
+const URL = process.env.REACT_APP_API_URL || 'https://f11.cl:8090';
 
 const productsApi = axios.create({
   baseURL: URL,
